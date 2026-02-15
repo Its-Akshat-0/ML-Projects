bbbc56b07a28755461f5232e348add35de7a6493
Akshat600 <devinefire@gmail.com>
2026-02-15 13:07:50 +0530

    updated ann

--- Deep learning/ANN/check_categories.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('Students Performance Dataset.csv')
-
-categorical_cols = df.select_dtypes(include=['object']).columns
-for col in categorical_cols:
-    print(f"\nCol: {col}")
-    print(df[col].unique())
-    print(f"Count: {len(df[col].unique())}")
